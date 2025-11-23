e968a5a9ee5637c06ab606252892287463512bfd
Vivek Parmar <122586277+VivekParmar20@users.noreply.github.com>
2025-11-23 11:45:45 +0530

    Refactor routes in Dashboard component
    Updated routing structure in Dashboard component for improved clarity.

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import Apps from "./Apps";
 import Funds from "./Funds";
 import Holdings from "./Holdings";
-
 import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
@@ -13,18 +12,22 @@ import { GeneralContextProvider } from "./GeneralContext";
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-container" >
+    <div className="dashboard-container">
       <GeneralContextProvider>
         <WatchList />
       </GeneralContextProvider>
+
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Summary />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/holdings" element={<Holdings />} />
-          <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
-          <Route path="/apps" element={<Apps />} />
+          {/* Default route (Dashboard Home) */}
+          <Route index element={<Summary />} />
+
+          {/* Other routes (no leading / ) */}
+          <Route path="orders" element={<Orders />} />
+          <Route path="holdings" element={<Holdings />} />
+          <Route path="positions" element={<Positions />} />
+          <Route path="funds" element={<Funds />} />
+          <Route path="apps" element={<Apps />} />
         </Routes>
       </div>
     </div>
